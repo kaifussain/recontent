a1fd852221af581ba809c3bb7d5e532d0ee4d4bd
k417hussain <k417hussain@gmail.com>
2024-09-29 23:56:02 +0530

    fixed issue fetching header.html

--- scripts/loaders.js
+++ scripts/loaders.js
@@ -1,5 +1,5 @@
 async function loadHeader() {
-    const response = await fetch('../components/header.html');
+    const response = await fetch('components/header.html');
     const headerHTML = await response.text();
     document.getElementById('header-container').innerHTML = headerHTML;
 }
