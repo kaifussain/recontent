56cab8f0265732ac0d2187b0f5ad99cf8ee45c8e
k417hussain <k417hussain@gmail.com>
2024-09-29 23:34:59 +0530

    first-setup

---
+++ scripts/loaders.js
@@ -0,0 +1,5 @@
+async function loadHeader() {
+    const response = await fetch('../components/header.html');
+    const headerHTML = await response.text();
+    document.getElementById('header-container').innerHTML = headerHTML;
+}
---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+const toggleButton = document.getElementById("theme-toggle");
+
+document.addEventListener("DOMContentLoaded", setTheme);
+
+function setTheme() {
+  const currentTheme = localStorage.getItem("theme") || "light";
+  document.documentElement.setAttribute("data-theme", currentTheme);
+}
+
+function toggleTheme() {
+  const currentTheme = localStorage.getItem("theme") || "dark";
+  // Set initial theme
+//   document.documentElement.setAttribute("data-theme", currentTheme);
+
+  // Toggle between light and dark themes
+  const newTheme = currentTheme === "light" ? "dark" : "light";
+  document.documentElement.setAttribute("data-theme", newTheme);
+
+  // Save the user's choice in localStorage
+  localStorage.setItem("theme", newTheme);
+}
